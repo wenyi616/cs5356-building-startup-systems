728f1c70ff175dc0f12c9c113c4299fdf418e833
wenyi616 <wenyiccchu@gmail.com>
2021-04-26 10:14:03 -0400

    complete frontend

--- frontend/iseeberg/src/App.js
+++ frontend/iseeberg/src/App.js
@@ -1,7 +1,12 @@
 // import logo from './logo.svg';
 import logo from './images/logo.png'; 
-import searchButton from './images/search-button.png'; 
 import './App.css';
+
+import React, { Component } from 'react'
+import Icon from '@mdi/react'
+import { mdiMagnify } from '@mdi/js';
+
+
 
 
 function App() {
@@ -40,31 +45,18 @@ function App() {
       <div class="container">
         <div class="search">
           <div class="columns is-vcentered reverse-columns">
-            <div class="column
-            is-10-mobile is-offset-1-mobile
-            is-10-tablet is-offset-1-tablet
-            is-5-desktop is-offset-1-desktop
-            is-5-widescreen is-offset-1-widescreen
-            is-5-fullhd is-offset-1-fullhd" data-aos="fade-down">
-              {/* <h1 class="title titled is-1 mb-6">
-                absurd illustrations that make sense
-              </h1>
-              <h2 class=" subtitled subtitle has-text-grey is-4 has-text-weight-normal is-family-sans-serif">
-                Lorem ipsum, dolor sit amet consectetur adipisicing elit. Laborum cupiditate dolorum vitae dolores
-                nesciunt totam magni quas.
-              </h2>
-              <div class="buttons">
-                <button class="button is-black">Download</button>
-                <button class="button">Subscribe</button>
-              </div> */}
-
+            <div class="column is-5-fullhd is-offset-1-fullhd
+            is-10-mobile is-offset-1-mobile is-10-tablet is-offset-1-tablet
+            is-5-desktop is-offset-1-desktop is-5-widescreen is-offset-1-widescreen" data-aos="fade-down">
               <input class="input is-rounded search-input" type="text" placeholder="WHERE ARE WE GOING NEXT?"></input>
-
             </div>
           
-            <div class="column search-button" data-aos="fade-down">
-              <img class="" src={searchButton}></img>
-            </div>
+
+            <button class="button search-button">
+              <span class="icon">
+              <Icon path={mdiMagnify} title="" color="#94B1DD" size="2em"/>
+              </span>
+            </button>
           </div>
           </div>
         </div>
